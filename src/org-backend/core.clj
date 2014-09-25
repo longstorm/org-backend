951c47358683aa6ad87e0c1739b167c1b1df4f23
Vic Goldfeld <vic@longstorm.org>
2014-09-25 17:46:14 -0300

    refactor org-backend.core/slurp

--- src/org-backend/core.clj
+++ src/org-backend/core.clj
@@ -1,12 +1,11 @@
-(ns org-backend.core) 
+(ns org-backend.core)
 
 (def *slurp-fn* (atom nil))
+(defn set-slurp-fn! [fn]
+  (reset! *slurp-fn* fn))
 
 (defn slurp [path]
   (try (@*slurp-fn* path)
        (catch js/Object e
          (throw (str "You must set-slurp-fn! with a function taking a valid "
-                     "string path and returning a string.")))))
-
-(defn set-slurp-fn! [fn]
-  (reset! *slurp-fn* fn))
+                     "string path.")))))

32db476273942f77d3ac1f8aeb0c025c5efc285c
Vic Goldfeld <vic@longstorm.org>
2015-08-30 18:39:24 -0300

    change leaf parsing heuristic and parse root props

--- src/org_backend/parser.cljs
+++ src/org_backend/parser.cljs
@@ -43,21 +43,15 @@
 
 (def org-file? org-file-name)
 
-(defn filter-out-empty [coll]
-  (remove #{"" "\n"} coll))
-
-(defn get-lines [s]
-  (filter-out-empty (strng/split s #"\n")))
-
 (defn filter-out-empty-chunks
   "The first chunk (the intro) is never filtered out since an empty
   intro is semantically meaningful and should be kept, whereas empty
-  chunks elsewhere are a calculated parsing error and should be here
-  corrected."
+  chunks elsewhere should be here corrected."
   [intro & chunks]
-  (into [intro] (filter-out-empty chunks)))
+  (->> (remove #{"" "\n"} chunks)
+       (into [intro])))
 
-(defn org-outline-chunks [level s]
+(defn parse-subtree-chunks [level s]
   (->> (org-delimiter level)
        re-pattern
        (strng/split s)
@@ -69,47 +63,83 @@
 
 (defn parse-properties [prop-lines]
   (reduce (fn [m line]
-            (let [[_ p val] (strng/split line #":")]
-              (assoc m (keyword p) (strng/trim val))))
-          {} (butlast (rest prop-lines))))
+            (let [[p val] (strng/split line #":")]
+              (assoc m (keyword p) (and (seq val) (strng/trim val)))))
+          {} prop-lines))
 
-(defn get-leafs-and-properties [intro]
-  (let [[line :as lines] (get-lines intro)]
-    (if-not (and (string? line)
-                 (re-find #"^\s*:(properties|PROPERTIES):" line))
-      [{} lines]
-      (update-in (split-at
-                  (->> lines
-                       (take-while #(not (re-find #"^\s*:(end|END):" %)))
-                       count inc inc)
-                  lines)
-                 [0] parse-properties))))
+(def subtree-props-parse-cfg
+  {:props-extract #(butlast (rest %))
+   :props-start #"^\s*:(properties|PROPERTIES):"
+   :props-keep #(not (re-find #"^\s*:(end|END):" %))
+   :props-prefix #"^\s*:"
+   :props-split-inc inc})
 
-(defn org-outline-shallow [level parsed-chunks]
-  (map #(let [[props leafs] (get-leafs-and-properties (second %))]
+(defn get-leafs-and-properties [intro cfg]
+  (let [[p & paras] (remove #{""} (strng/split intro #"\n\n"))]
+    (if-not (and (string? p) (re-find (:props-start cfg) p))
+      [{} (cond (nil? paras) []
+                (empty? p) paras
+                :else (cons p paras))]
+      (let [lines (strng/split p #"\n")
+            [prop leafs]
+            (split-at (->> lines (take-while (:props-keep cfg))
+                           count ((:props-split-inc cfg)))
+                      lines)]
+        [(-> (map #(strng/replace % (:props-prefix cfg) "") prop)
+             ((:props-extract cfg))
+             parse-properties)
+         (cons (strng/join " " leafs) paras)]))))
+
+(defn parse-subtree-shallow [level parsed-chunks]
+  (map #(let [[props leafs] (get-leafs-and-properties
+                             (second %) subtree-props-parse-cfg)]
           {:headline (first %)
            :properties props
            :level (inc level)
            :leafs leafs})
        parsed-chunks))
 
+(defn parse-subtree
+  "This is an inner recursion version of the `org-outline` function,
+  and the only difference is the properties parse syntax."
+  ([level shallow title s]
+   (let [[intro & chunks] (parse-subtree-chunks level s)
+         subtrees (map parse-chunk chunks)
+         [props leafs] (get-leafs-and-properties
+                        intro subtree-props-parse-cfg)]
+     {:headline title
+      :properties props
+      :level level
+      :leafs leafs
+      :subtrees
+      (if shallow
+        (parse-subtree-shallow level subtrees)
+        (map (partial apply (partial parse-subtree (inc level) false))
+             subtrees))})))
+
 (defn org-outline
   ([path] (org-outline 0 false (org-file-name path) (slurp path)))
   ([path shallow] (org-outline 0 true (org-file-name path) (slurp path)))
   ([level title s] (org-outline level false title s))
   ([level shallow title s]
-     (let [[intro & chunks] (org-outline-chunks level s)
-           subtrees (map parse-chunk chunks)
-           [props leafs] (get-leafs-and-properties intro)]
-       {:headline title
-        :properties props
-        :level level
-        :leafs leafs
-        :subtrees (if shallow
-                    (org-outline-shallow level subtrees)
-                    (map (partial apply
-                                  (partial org-outline (inc level) false))
-                         subtrees))})))
+   (let [[intro & chunks] (parse-subtree-chunks level s)
+         subtrees (map parse-chunk chunks)
+         root-prop-line #"^\s*#\+"
+         [props leafs] (get-leafs-and-properties
+                        intro {:props-extract identity
+                               :props-start root-prop-line
+                               :props-keep (partial re-find root-prop-line)
+                               :props-prefix root-prop-line
+                               :props-split-inc identity})]
+     {:headline title
+      :properties props
+      :level level
+      :leafs leafs
+      :subtrees
+      (if shallow
+        (parse-subtree-shallow level subtrees)
+        (map (partial apply (partial parse-subtree (inc level) false))
+             subtrees))})))
 
 (defn org-outline-empty
   ([] (org-outline-empty ""))
